929809c0d9d3219ff08722ee510a36874212fb3a
Igor do Nascimento Andrade <igornascimento060@gmail.com>
2020-09-27 16:08:02 -0300

    Adiconado no module o router para funcioar as rotas nos botoes

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { PostComponent } from './post/post.component';
 import { HeaderToolbarComponent } from './header-toolbar/header-toolbar.component';
 import { FooterToolbarComponent} from './footer-toolbar/footer-toolbar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
     declarations: [
@@ -13,6 +14,9 @@ import { FooterToolbarComponent} from './footer-toolbar/footer-toolbar.component
          PostComponent,
          HeaderToolbarComponent,
          FooterToolbarComponent
+        ],
+    imports: [
+        RouterModule
         ]
 })
 
