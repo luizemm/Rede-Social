e30cd36e0bbf3a44e2515a5ace91dcd11c14d878
luizemm <luizemmlz@gmail.com>
2020-10-03 13:54:00 -0300

    Paginas new-post e comments adicionam um novo post com o nome e imagem do objeto person que está logado (de acordo com o email que está na variavel global)

--- src/app/pages/comments/comments.page.ts
+++ src/app/pages/comments/comments.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Person } from 'src/app/models/person.model';
 import { post } from 'src/app/models/post.module';
+import { PersonService } from 'src/app/services/person.service';
 import { PostService } from 'src/app/services/post.service';
 
 @Component({
@@ -11,24 +13,28 @@ import { PostService } from 'src/app/services/post.service';
 export class CommentsPage implements OnInit {
 
   post : post;
+  person : Person;
   listPost : post[] = [];
   comment : post = new post();
 
   constructor(private postService : PostService,
-    private route : ActivatedRoute) {
-      this.post = this.postService.getPostById(Number.parseInt(this.route.snapshot.paramMap.get('id')));
-      for(let comment of this.post.comments){
-        this.listPost.push(postService.getPostById(comment));
-      }
+    private personService : PersonService,
+    private route : ActivatedRoute) 
+  {
+    this.post = this.postService.getPostById(Number.parseInt(this.route.snapshot.paramMap.get('id')));
+    this.person = this.personService.getPersonByEmail(localStorage['login']);
+    for(let comment of this.post.comments){
+      this.listPost.push(postService.getPostById(comment));
     }
+  }
 
   ngOnInit() {
     
   }
 
   onSubmit(){
-    this.comment.name = 'teste';
-    this.comment.picture = '../../assets/pictures/default profile.jpg';
+    this.comment.name = this.person.name;
+    this.comment.picture = this.person.picture;
     this.comment.time = '1h';
     this.postService.addComment(this.post.id, this.comment);
     this.listPost.unshift(this.postService.getPostById(this.post.comments[0]))
--- src/app/pages/new-post/new-post.page.ts
+++ src/app/pages/new-post/new-post.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Person } from 'src/app/models/person.model';
 import { post } from 'src/app/models/post.module';
+import { PersonService } from 'src/app/services/person.service';
 import { PostService } from 'src/app/services/post.service';
 
 @Component({
@@ -10,18 +12,23 @@ import { PostService } from 'src/app/services/post.service';
 })
 export class NewPostPage implements OnInit {
 
-  private picture : String = '../../../assets/pictures/default profile.jpg';
+  person : Person;
   post : post = new post();
 
-  constructor(private postService : PostService, private route : Router) { }
+  constructor(private postService : PostService,
+     private personService : PersonService,
+     private route : Router) 
+  { 
+    this.person = personService.getPersonByEmail(localStorage['login']);
+  }
 
   ngOnInit() {
     
   }
 
   onSubmit(){
-    this.post.name = 'teste';
-    this.post.picture = '../../assets/pictures/default profile.jpg';
+    this.post.name = this.person.name;
+    this.post.picture = this.person.picture;
     this.post.time = '1h';
     this.postService.addPost(this.post);
     this.post = new post();
